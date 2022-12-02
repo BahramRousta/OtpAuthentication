2d91f6b8047d51b1bb6bf4b60800529072e958c9
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2022-12-02 14:25:20 +0330

    add include_docs_urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('auth/', include('drf_social_oauth2.urls', namespace='drf')),
@@ -8,9 +9,10 @@ urlpatterns = [
     path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
     path('api-accounts/', include('accounts.urls')),
+    path('docs/', include_docs_urls(title='AuthenticationAPI')),
     path('schema/', get_schema_view(
         title='AuthenticationAPI',
         description='API for the AuthenticationAPI',
         version='1.0.0',
-    ), name='openapi-schema'),
+    ), name='openapi-schema')
 ]
