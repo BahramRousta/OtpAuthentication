d0412858268f9fb99cd7ef7148b532d3bb024554
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2022-12-30 22:47:09 +0330

    change refresh token time life to minutes mode

--- authentication/settings.py
+++ authentication/settings.py
@@ -160,7 +160,7 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = [
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=1439),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
 
