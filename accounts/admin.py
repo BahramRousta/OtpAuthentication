5d6476902f93c23136a89ae2e26c079fc5d10614
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2022-12-30 15:38:03 +0330

    change custom user model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,15 +1,5 @@
 from django.contrib import admin
 from .models import CustomUser, Otp
-from django.contrib.auth.admin import UserAdmin
-from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-
-class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = CustomUser
-    list_display = ["username", "email", "phone_number"]
-
-
-admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(CustomUser)
 admin.site.register(Otp)
