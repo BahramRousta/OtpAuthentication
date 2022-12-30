4169b36f47d03e163979584d408e921129dd79a1
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2022-12-30 18:41:24 +0330

    change create superuser function

--- accounts/models.py
+++ accounts/models.py
@@ -10,22 +10,20 @@ from rest_framework_simplejwt.tokens import RefreshToken
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, username, email, password=None):
+    def create_user(self, username, password=None):
         if username is None:
             raise TypeError('Users should have a username')
-        if email is None:
-            raise TypeError('Users should have a Email')
 
-        user = self.model(username=username, email=self.normalize_email(email))
+        user = self.model(username=username)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, username, email, password=None):
+    def create_superuser(self, username, password=None):
         if password is None:
             raise TypeError('Password should not be none')
 
-        user = self.create_user(username, email, password)
+        user = self.create_user(username, password)
         user.is_superuser = True
         user.is_staff = True
         user.save()
