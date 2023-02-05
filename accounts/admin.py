f96d465adf3a64e144866349f7eda8777a097d0b
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2023-02-05 13:38:53 +0330

    add static dir to gitignore

--- accounts/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser, Otp
-
-admin.site.register(CustomUser)
-admin.site.register(Otp)
--- social_authentication/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import GoogleSocialAuthView
-
-urlpatterns = [
-    path('google-auth/', GoogleSocialAuthView.as_view()),
-]
